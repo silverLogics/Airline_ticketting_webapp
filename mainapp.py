7c2bef4158a8ee32ade9fd1ee0dea9fe6ef71991
elijahal18 <eal510@nyu.edu>
2021-11-16 20:59:43 -0500

    adding a start page and register option

--- mainapp.py
+++ mainapp.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template  # importing the render_template functi
 
 app = Flask(__name__)
 # home route
-@app.route("/showlogin")
+@app.route("/")
 def hello():
-    return render_template('login.html')
+    return render_template('startpage.html')
+    
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+@app.route('/registerCustomer')
+def registerCustomer():
+  return render_template('registerCust.html')
 
 app.run(debug = True)
